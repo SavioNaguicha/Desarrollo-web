f9119fd42fd309dd547ab4cb0d5710bd1908c493
SavioNaguicha <savionaguicha@hotmail.com>
2026-07-12 22:48:37 -0500

    Semana 7

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,17 @@
 function Saludar() {
     alert ("Hola bienvenidos")
 }
+
+// validacion de correo 
+const emailInput = document.getElementById("email");
+const feedback = document.getElementById("feedback");
+emailInput.addEventListener("input", () => {
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (emailPattern.test(emailInput.value)) {
+feedback.textContent = "Correo válido";
+feedback.style.color = "green";
+} else {
+feedback.textContent = "Correo no válido";
+feedback.style.color = "red";
+}
+});
